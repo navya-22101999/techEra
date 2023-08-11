7e8fa864b13e5c78815b915bd29e995b0ddf0990
navya-22101999 <navyatammanaboina@gmail.com>
2023-08-11 06:45:53 +0000

    techera

---
+++ src/components/Course/index.js
@@ -0,0 +1,19 @@
+import {Component} from 'react'
+import {Link} from 'react-router-dom'
+
+export default class Course extends Component {
+  render() {
+    const {details} = this.props
+    const {id, logoUrl, name} = details
+    return (
+      <Link to={`courses/${id}`}>
+        <li className="listItem">
+          <div>
+            <img src={logoUrl} alt={name} />
+          </div>
+          <p className="LinkItems">{name}</p>
+        </li>
+      </Link>
+    )
+  }
+}
